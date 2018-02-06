b0d0f638992242fab87a673b75ebf9908532ef12
ambuilding <explorewo@gmail.com>
2018-02-06 10:39:57 +0800

    plot our results and look for differences across each translation

--- harvardResearch/langProcess/hamlet.py
+++ harvardResearch/langProcess/hamlet.py
@@ -66,3 +66,37 @@ for i in range(hamlets.shape[0]):
     sub_data = summarize_text(language, text)
     #print(sub_data)
     grouped_data = grouped_data.append(sub_data)
+
+
+colors = {"Portuguese": "green", "English": "blue", "German": "red"}
+markers = {"frequent": "o","infrequent": "s", "unique": "^"}
+import matplotlib.pyplot as plt
+for i in range(grouped_data.shape[0]):
+    row = grouped_data.iloc[i]
+    plt.plot(row.mean_word_length, row.num_words,
+        marker=markers[row.frequency],
+        color = colors[row.language],
+        markersize = 10
+    )
+
+color_legend = []
+marker_legend = []
+for color in colors:
+    color_legend.append(
+        plt.plot([], [],
+        color=colors[color],
+        marker="o",
+        label = color, markersize = 10, linestyle="None")
+    )
+for marker in markers:
+    marker_legend.append(
+        plt.plot([], [],
+        color="k",
+        marker=markers[marker],
+        label = marker, markersize = 10, linestyle="None")
+    )
+
+plt.legend(numpoints=1, loc = "upper left")
+plt.xlabel("Mean Word Length")
+plt.ylabel("Number of Words")
+plt.show()
